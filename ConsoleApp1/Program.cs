bea6b46d9cac888013c8c1e6922bc7c62e9f60f6
Fibiskus <viktoriazdeoruk@gmail.com>
2020-05-31 01:32:52 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            string file = "Hi is , driving a red car";
+            string number = file.Split(',')[0].Trim();
+            Console.WriteLine(number.Replace("red", "").Replace("  ", " "));
+        }
+    }
+}
